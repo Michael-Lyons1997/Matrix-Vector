f107ee5ab9363d19a453ac098c2075b5bb060317
(Student) - Michael Lyons <C00217557@ITCARLOW.IE>
2018-01-11 12:29:31 +0000

    finish matirx

--- MatrixAndVector/MatrixAndVector/Matrix.cpp
+++ MatrixAndVector/MatrixAndVector/Matrix.cpp
@@ -32,3 +32,93 @@ Matrix::Matrix(double newA11, double newA12, double newA13, double newA21, doubl
 Matrix::~Matrix()
 {
 }
+
+void Matrix::setA11(double newA11)
+{
+	A11 = newA11;
+}
+
+void Matrix::setA12(double newA12)
+{
+	A12 = newA12;
+}
+
+void Matrix::setA13(double newA13)
+{
+	A13 = newA13;
+}
+
+void Matrix::setA21(double newA21)
+{
+	A21 = newA21;
+}
+
+void Matrix::setA22(double newA22)
+{
+	A22 = newA22;
+}
+
+void Matrix::setA23(double newA23)
+{
+	A23 = newA23;
+}
+
+void Matrix::setA31(double newA31)
+{
+	A31 = newA31;
+}
+
+void Matrix::setA32(double newA32)
+{
+	A32 = newA32;
+}
+
+void Matrix::setA33(double newA33)
+{
+	A33 = newA33;
+}
+
+double Matrix::getA11()
+{
+	return A11;
+}
+
+double Matrix::getA12()
+{
+	return A12;
+}
+
+double Matrix::getA13()
+{
+	return A13;
+}
+
+double Matrix::getA21()
+{
+	return A21;
+}
+
+double Matrix::getA22()
+{
+	return A22;
+}
+
+double Matrix::getA23()
+{
+	return A23;
+}
+
+double Matrix::getA31()
+{
+	return A31;
+}
+
+double Matrix::getA32()
+{
+	return A32;
+}
+
+double Matrix::getA33()
+{
+	return A33;
+}
--- MatrixAndVector/MatrixAndVector/Matrix.h
+++ MatrixAndVector/MatrixAndVector/Matrix.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Vector.h"
 class Matrix
 {
 public:
@@ -7,6 +8,25 @@ public:
 		double newA21, double newA22, double newA23,
 		double newA31, double newA32, double newA33);
 	~Matrix();
+	void setA11(double newA11);
+	void setA12(double newA12);
+	void setA13(double newA13);
+	void setA21(double newA21);
+	void setA22(double newA22);
+	void setA23(double newA23);
+	void setA31(double newA31);
+	void setA32(double newA32);
+	void setA33(double newA33);
+
+	double getA11();
+	double getA12();
+	double getA13();
+	double getA21();
+	double getA22();
+	double getA23();
+	double getA31();
+	double getA32();
+	double getA33();
 
 protected:
 	double A11;
